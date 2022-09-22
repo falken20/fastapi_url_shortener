a2cc30c669c08429dc01d13c0c0a10e3aac6b686
Richi <r.rodriguez@bbva.com>
2022-09-22 19:41:15 +0200

    Adding sh file and first endpoint

---
+++ url_shortener.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Create virtual environment..."
+#python -m venv venv
+
+echo "Access to virtual environment..."
+#source ./venv/bin/activate
+
+echo "Running app..."
+# The --reload flag makes sure that your server will reload automatically when you save your application’s code
+uvicorn url_shortener.main:app --reload
+
+echo "Deactivate virtual environment..."
+#deactivate
--- url_shortener/main.py
+++ url_shortener/main.py
@@ -1,10 +1,16 @@
 # by Richi Rod AKA @richionline / falken20
 # url_shortener/main.py
 
+# FastAPI creates documentation of your API endpoints for you. 
+# Go check it out in your browser at: 
+# http://127.0.0.1:8000/docs 
+# http://127.0.0.1:8000/redoc
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+@app.get("/home")
+def main():
     return "Welcome to the URL Shortener API"
