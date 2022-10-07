9ed319c0c2d43335e30cc062b975975e5246a2ed
Richi <r.rodriguez@bbva.com>
2022-10-07 08:47:25 +0200

    Adding forward a shortened url

--- url_shortener/main.py
+++ url_shortener/main.py
@@ -8,6 +8,7 @@
 
 import secrets
 from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import RedirectResponse
 import validators  # To validate URLs, email addresses, IP addresses, etc.
 from sqlalchemy.orm import Session
 
@@ -65,3 +66,16 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
