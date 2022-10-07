d78f4a42f802f4f950cb4233ab16f692404b072d
Richi <r.rodriguez@bbva.com>
2022-10-07 08:45:31 +0200

    Adding forward a shortened url

--- url_shortener/main.py
+++ url_shortener/main.py
@@ -7,7 +7,7 @@
 # http://127.0.0.1:8000/redoc
 
 import secrets
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 import validators  # To validate URLs, email addresses, IP addresses, etc.
 from sqlalchemy.orm import Session
 
@@ -15,9 +15,11 @@ from . import schemas, models
 from .database import SessionLocal, engine
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine) # If not exists it'll be created
+models.Base.metadata.create_all(bind=engine)  # If not exists it'll be created
 
 # It will create and yield new database sessions with each request
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -36,13 +38,18 @@ def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
 
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
-    # With get_db() into Depends(), you establish a database session 
+    # With get_db() into Depends(), you establish a database session
     # for the request and close the session when the request is finished.
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
-    
+
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     key = "".join(secrets.choice(chars) for _ in range(5))
     secret_key = "".join(secrets.choice(chars) for _ in range(8))
@@ -52,7 +59,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db.add(db_url)
     db.commit()
     db.refresh(db_url)
-    # Add key and secret_key to db_url to match the required URLInfo schema 
+    # Add key and secret_key to db_url to match the required URLInfo schema
     # that you need to return at the end of the function.
     db_url.url = key
     db_url.admin_url = secret_key
