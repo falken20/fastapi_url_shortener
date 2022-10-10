4129c91866f32cfab8c5964492e4c08274d6271a
Richi <r.rodriguez@bbva.com>
2022-10-10 08:56:30 +0200

    Refactoring code

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -9,7 +9,7 @@ class URL(Base):
     __tablename__ = "urls"
 
     id = Column(Integer, primary_key=True)
-    key = Column(String, unique=True, index=True)
+    key = Column(String, unique=True, index=True) # Short url
     secret_key = Column(String, unique=True, index=True)
     target_url = Column(String, index=True)
     is_active = Column(Boolean, default=True)
